24878f8e31e747edc1f833ef9ec5dc63d896b3a9
Lucrecious <luca.duran@hotmail.com>
2022-10-17 14:15:16 -0400

    Add banan barrage attack

---
+++ src/resources/arpeegee_pins/actions/slice_barrage_banan_chopped.gd
@@ -0,0 +1,43 @@
+extends Node2D
+
+const MIN_HITS := 2
+const MAX_HITS := 7
+
+
+func pin_action() -> PinAction:
+	return load('res://src/resources/actions/slice_barrage_banan_chopped.tres') as PinAction
+
+func run(actioner: Node2D, target: Node2D, object: Object, callback: String) -> void:
+	var animation := create_tween()
+	animation.tween_interval(0.5)
+	
+	var target_box := NodE.get_child(target, REferenceRect) as REferenceRect
+	var target_center := target_box.global_rect().get_center()
+	
+	var sprite_switcher := NodE.get_child(actioner, SpriteSwitcher) as SpriteSwitcher
+	var slices_node := sprite_switcher.sprite('idle')
+	
+	var stats := NodE.get_child(actioner, ModifiedPinStats) as ModifiedPinStats
+	var damage_amount := ActionUtils.damage_with_factor(stats.attack, 0.3)
+	var attack_times := MIN_HITS + (randi() % (MAX_HITS - MIN_HITS))
+	var slice_order := slices_node.get_children()
+	for i in attack_times:
+		var slice := slice_order[i] as Node2D
+		animation.tween_property(slice, 'global_position', target_center, 0.75)\
+				.set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_EXPO)
+		ActionUtils.add_attack(animation, actioner, target, damage_amount)
+		
+		animation.tween_callback(VFX, 'physical_impactv', [target, target_center])
+		
+		animation.tween_callback(slice, 'set', ['visible', false])
+		
+		animation.tween_interval(0.35)
+	
+	animation.tween_interval(1.0)
+	
+	for i in attack_times:
+		var slice := slice_order[i] as Node2D
+		animation.tween_callback(slice, 'set', ['global_position', slice.global_position])
+		animation.tween_callback(slice, 'set', ['visible', true])
+	
+	animation.tween_callback(object, callback)
