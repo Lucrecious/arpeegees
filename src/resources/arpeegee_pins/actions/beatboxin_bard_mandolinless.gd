c2cae63a1ee996b8cadf6e88037a40d2c539c68d
Lucrecious <luca.duran@hotmail.com>
2022-10-18 19:00:24 -0400

    Add beatboxin to bard

---
+++ src/resources/arpeegee_pins/actions/beatboxin_bard_mandolinless.gd
@@ -0,0 +1,39 @@
+extends Node2D
+
+signal text_triggered(narration_key)
+
+func pin_action() -> PinAction:
+	return preload('res://src/resources/actions/beatboxin_bard_mandolinless.tres')
+
+func run(actioner: Node2D, object: Object, callback: String) -> void:
+	var scream_action := NodE.get_sibling_by_name(self, 'Scream') as Node2D
+	assert(scream_action)
+	scream_action.boosted = true
+	
+	var animation := create_tween()
+	animation.tween_interval(0.35)
+	
+	var sprite_switcher := NodE.get_child(actioner, SpriteSwitcher) as SpriteSwitcher
+	animation.tween_callback(sprite_switcher, 'change', ['beatbox'])
+	
+	var root_sprite := Components.root_sprite(actioner)
+	var skew_stepper := JuiceSteppers.SkewBackAndForth.new(animation, root_sprite.material)
+	skew_stepper.offset = 0.05
+	skew_stepper.offset_to_home_sec = 0.2
+	skew_stepper.between_offsets_sec = 0.5
+	
+	for i in 6:
+		skew_stepper.step()
+	
+	skew_stepper.finish()
+	
+	ActionUtils.add_text_trigger(animation, self, 'NARRATOR_BEATBOXIN_USE')
+	
+	animation.tween_interval(1.0)
+	
+	animation.tween_callback(sprite_switcher, 'change', ['idle'])
+	
+	animation.tween_interval(0.5)
+	animation.tween_callback(object, callback)
+
+
