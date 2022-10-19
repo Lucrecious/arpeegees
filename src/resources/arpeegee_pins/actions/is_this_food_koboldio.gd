755aafb7d990ac424f3be4ac4992252e5fc75ff8
Lucrecious <luca.duran@hotmail.com>
2022-10-19 19:37:26 -0400

    Add is this food to koboldio with mandolin

---
+++ src/resources/arpeegee_pins/actions/is_this_food_koboldio.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+func pin_action() -> PinAction:
+	return preload('res://src/resources/actions/is_this_food_koboldio.tres')
+
+func run(actioner: Node2D, targets: Array, object: Object, callback: String) -> void:
+	var animation := create_tween()
+	animation.tween_interval(0.35)
+	
+	var sprite_switcher := NodE.get_child(actioner, SpriteSwitcher) as SpriteSwitcher
+	animation.tween_callback(sprite_switcher, 'change', ['isthisfood'])
+	
+	animation.tween_interval(1.0)
+	animation.tween_callback(sprite_switcher, 'change', ['idle'])
+	
+	animation.tween_callback(object, callback)
