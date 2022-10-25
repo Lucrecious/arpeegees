70aaf5f8cfa17785277796991aa5dc1eab416c78
Lucrecious <luca.duran@hotmail.com>
2022-10-25 14:10:32 -0400

    Fix y sort bug

--- src/game/battle/y_sort_during_actions.gd
+++ src/game/battle/y_sort_during_actions.gd
@@ -40,7 +40,12 @@ func _process(delta: float) -> void:
 	if Engine.get_idle_frames() % 5 != 0:
 		return
 	
+	if _tracking_pins.empty():
+		return
+	
 	_tracking_pins.sort_custom(self, '_sort_based_on_y')
+	var control_sibling_count := _tracking_pins[0].get_parent().get_parent().get_child_count() as int
+	var pins_count := _tracking_pins.size()
 	for i in _tracking_pins.size():
 		var pin := _tracking_pins[i] as ArpeegeePinNode
 		var control := pin.get_parent() as Control
@@ -48,7 +53,7 @@ func _process(delta: float) -> void:
 		if not control:
 			continue
 		
-		control.get_parent().move_child(control, i)
+		control.get_parent().move_child(control, control_sibling_count - (pins_count - i))
 
 func _sort_based_on_y(pin: ArpeegeePinNode, other: ArpeegeePinNode) -> bool:
 	return pin.global_position.y < other.global_position.y
