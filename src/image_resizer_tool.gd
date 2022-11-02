9fa12a302ada3b011bb10588733a6c2b43d40f83
Lucrecious <luca.duran@hotmail.com>
2022-11-02 15:20:14 -0400

    Resizing script

--- src/image_resizer_tool.gd
+++ src/image_resizer_tool.gd
@@ -17,7 +17,8 @@ func _run_set(value: bool) -> void:
 		
 		var texture_path := ProjectSettings.globalize_path(texture.resource_path)
 		var scale_percent := sprite.scale.x * 3.0
-		var command := 'magick %s -resize %d%% -filter Lanczos %s' % [texture_path, scale_percent * 100.0, texture_path]
+		var command := 'magick'
+		var args := [ texture_path, '-resize %d%%' % [scale_percent * 100.0], '-filter Lanczos', texture_path ]
 		
 		OS.execute(command, [])
 		
