f6bea8af80146abe1b522d0335df145c56e1e737
Lucrecious <luca.duran@hotmail.com>
2022-11-22 15:25:28 -0500

    Add spittin goo to blobbo with sword

--- src/resources/arpeegee_pins/actions/spittin_goo_blobbo_sparkling.gd
+++ src/resources/arpeegee_pins/actions/spittin_goo_blobbo_sparkling.gd
@@ -3,10 +3,23 @@ extends Node2D
 const MIN_HITS := 2
 const MAX_HITS := 5
 
+enum Type {
+	Sparkling,
+	Sword
+}
+
+export(Type) var type := Type.Sparkling
+
 onready var _goo := $Goo as Node2D
 
 func pin_action() -> PinAction:
-	return preload('res://src/resources/actions/spittin_goo_blobbo_sparkling.tres') as PinAction
+	if type == Type.Sparkling:
+		return preload('res://src/resources/actions/spittin_goo_blobbo_sparkling.tres') as PinAction
+	elif type == Type.Sword:
+		return preload('res://src/resources/actions/spittin_goo_blobbo_sword.tres') as PinAction
+	else:
+		assert(false)
+		return preload('res://src/resources/actions/spittin_goo_blobbo_sparkling.tres')
 
 func run(actioner: Node2D, target: Node2D, object: Object, callback: String) -> void:
 	var animation := create_tween()
@@ -25,7 +38,8 @@ func run(actioner: Node2D, target: Node2D, object: Object, callback: String) -> 
 				.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
 		TweenJuice.skew(animation, material, -0.3, 0.0, 0.1)
 		
-		animation.tween_callback(sprite_switcher, 'change', ['shootgoo'])
+		if type == Type.Sparkling:
+			animation.tween_callback(sprite_switcher, 'change', ['shootgoo'])
 		
 		animation.tween_callback(_goo, 'set', ['visible', true])
 		animation.tween_property(_goo, 'global_position', target_position, 0.1)
