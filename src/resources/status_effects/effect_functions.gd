dac5ac4a2f9e09904f350a2b19f91b84aaebabd5
Lucrecious <luca.duran@hotmail.com>
2022-12-12 13:24:28 -0500

    Add burn sound

--- src/resources/status_effects/effect_functions.gd
+++ src/resources/status_effects/effect_functions.gd
@@ -209,6 +209,7 @@ class BurnEffect extends Node:
 		var burn_amount := ActionUtils.damage_with_factor(attack_based, 0.3)
 		animation.tween_callback(damage_receiver, 'damage',
 				[burn_amount, PinAction.AttackType.Normal, false, null])
+		animation.tween_callback(Sounds, 'play', ['DamageBurn'])
 		
 		animation.tween_callback(self, 'call_deferred',
 				['emit_signal', 'start_turn_effect_finished'])
