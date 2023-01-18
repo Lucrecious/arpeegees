9b6cd86372c30069461d09fa42e7d68c0255d024
Lucrecious <luca.duran@hotmail.com>
2023-01-18 16:47:56 -0500

    Add new website stuff, and resize below window size properly

--- src/stretch_adjust.gd
+++ src/stretch_adjust.gd
@@ -1,29 +1,14 @@
 extends Node
 
-const min_stretch := 1.5
-const max_stretch := 3.0
+const MIN_STRETCH := 1.5
+const MIN_WINDOW_SIZE_X := 1240
 
 var _current_stretch := 1.0
 
-#onready var _boundaries := NodE.get_sibling_by_name(self, 'Main')\
-#		.get_node('Boundaries') as Control
-#onready var _original_size_x := _boundaries.rect_size.x
-
 func _ready() -> void:
+	get_viewport().connect('size_changed', self, '_on_viewport_size_changed')
+	
 	_set_stretch(1.5)
-	#get_viewport().connect('size_changed', self, '_on_size_changed')
-	#_update_stretch()
-
-#func _on_size_changed() -> void:
-#	_update_stretch()
-#
-#func _update_stretch():
-#	OS.min_window_size = Vector2(300.0, 100.0)
-#	var factor := _boundaries.get_viewport_rect().size.x / _boundaries.rect_size.x
-#	factor *= _current_stretch
-#	factor = clamp(factor, min_stretch, max_stretch)
-#
-#	_set_stretch(factor)
 
 func _set_stretch(factor: float) -> void:
 	if is_equal_approx(factor, _current_stretch):
@@ -34,4 +19,29 @@ func _set_stretch(factor: float) -> void:
 	get_tree().set_screen_stretch(
 			SceneTree.STRETCH_MODE_DISABLED,
 			SceneTree.STRETCH_ASPECT_KEEP_HEIGHT,
-			Vector2(300.0, 100.0), factor)
+			Vector2.ZERO, factor)
+
+func _on_viewport_size_changed() -> void:
+	adapt(get_viewport().get_size_override().x)
+
+var _current_size := 0.0
+var _is_stretching := false
+func adapt(size: float) -> void:
+	size = OS.window_size.x
+	
+	if _is_stretching:
+		return
+	
+	if abs(_current_size - size) < 50:
+		return
+	
+	_current_size = size
+	_is_stretching = true
+	
+	if size > MIN_WINDOW_SIZE_X:
+		_set_stretch(MIN_STRETCH)
+	else:
+		var ratio := size / MIN_WINDOW_SIZE_X
+		_set_stretch(ratio * MIN_STRETCH)
+	
+	_is_stretching = false
